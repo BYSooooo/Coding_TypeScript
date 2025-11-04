68859d02ec0bf487f539fc64c0001a994be757a9
BYSooooo <avera0128@gmail.com>
2025-11-04 23:59:20 +0900

    [251104]Nomad Coder NestJS Lecture

---
+++ NestJS-making-API/Lecture02_07/src/movies/movies.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, RawBody, Req, Res } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDTO } from './dto/create-movie.dto';
+import { UpdateMovieDTO } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+
+    constructor(private readonly moviesService : MoviesService) {}
+
+    @Get()
+    getAll(@Req() req, @Res() res) : Movie[]{
+        
+        return this.moviesService.getAll();
+    }
+
+    @Get("/:id")
+    getOne(@Param('id') movieId: number) : Movie | undefined{
+        return this.moviesService.getOne(movieId);
+    }
+
+    @Post()
+    create(@Body() movieData : CreateMovieDTO) {
+        return this.moviesService.create(movieData);
+    }
+
+    @Delete("/:id")
+    remove(@Param('id') movieId : number) {
+        return this.moviesService.deleteOne(movieId);
+    }
+    
+    @Patch("/:id")
+    patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDTO) {
+        return this.moviesService.update(movieId, updateData);
+    }
+}
